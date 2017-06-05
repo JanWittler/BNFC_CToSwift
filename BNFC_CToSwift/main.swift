38cd160288752b691ecac89b7f54a5eb8ba71517
Jan Wittler <jan.wittler@innomos.com>
2017-06-05 16:13:08 +0200

    Initial Commit

---
+++ BNFC_CToSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BNFC_CToSwift
+//
+//  Created by Jan Wittler on 05.06.17.
+//  Copyright © 2017 Jan Wittler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
